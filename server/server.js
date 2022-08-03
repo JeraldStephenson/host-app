e87ffcb51a6accd7b779a083adcb2f2cdf7b0c73
JeraldStephenson <62264085+JeraldStephenson@users.noreply.github.com>
2022-08-03 10:14:42 -0700

    copied host-app/server into this repo for testing

---
+++ server/server.js
@@ -0,0 +1,162 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const cors = require('cors');
+
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull,
+} = require('graphql');
+
+const {
+  grapherrql,
+  eventsHandler,
+  serveGrapherrql,
+} = require('../../functions/index');
+const PORT = '3001';
+const app = express();
+
+app.use(cors());
+//Needed for SSE
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const authors = [
+  { id: 1, name: 'J. K. Rowling' },
+  { id: 2, name: 'J. R. R. Tolkien' },
+  { id: 3, name: 'Brent Weeks' },
+];
+
+const books = [
+  { id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1 },
+  { id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
+  { id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1 },
+  { id: 4, name: 'The Fellowship of the Ring', authorId: 2 },
+  { id: 5, name: 'The Two Towers', authorId: 2 },
+  { id: 6, name: 'The Return of the King', authorId: 2 },
+  { id: 7, name: 'The Way of Shadows', authorId: 3 },
+  { id: 8, name: 'Beyond the Shadows', authorId: 3 },
+];
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  description: 'This represents a book written by an author',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLInt) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    authorId: { type: new GraphQLNonNull(GraphQLInt) },
+    author: {
+      type: AuthorType,
+      resolve: (book) => {
+        return authors.find((author) => author.id === book.authorId);
+      },
+    },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  description: 'This represents a author of a book',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLInt) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve: (author) => {
+        return books.filter((book) => book.authorId === author.id);
+      },
+    },
+  }),
+});
+
+const RootQueryType = new GraphQLObjectType({
+  name: 'Query',
+  description: 'Root Query',
+  fields: () => ({
+    book: {
+      type: BookType,
+      description: 'A Single Book',
+      args: {
+        id: { type: GraphQLInt },
+      },
+      resolve: (parent, args) => books.find((book) => book.id === args.id),
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      description: 'List of All Books',
+      resolve: () => books,
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      description: 'List of All Authors',
+      resolve: () => authors,
+    },
+    author: {
+      type: AuthorType,
+      description: 'A Single Author',
+      args: {
+        id: { type: GraphQLInt },
+      },
+      resolve: (parent, args) =>
+        authors.find((author) => author.id === args.id),
+    },
+  }),
+});
+
+const RootMutationType = new GraphQLObjectType({
+  name: 'Mutation',
+  description: 'Root Mutation',
+  fields: () => ({
+    addBook: {
+      type: BookType,
+      description: 'Add a book',
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        authorId: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      resolve: (parent, args) => {
+        const book = {
+          id: books.length + 1,
+          name: args.name,
+          authorId: args.authorId,
+        };
+        books.push(book);
+        return book;
+      },
+    },
+    addAuthor: {
+      type: AuthorType,
+      description: 'Add an author',
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve: (parent, args) => {
+        const author = { id: authors.length + 1, name: args.name };
+        authors.push(author);
+        return author;
+      },
+    },
+  }),
+});
+
+const schema = new GraphQLSchema({
+  query: RootQueryType,
+  mutation: RootMutationType,
+});
+
+//GraphQL base endpoint. Middleware called before GraphQL processing to capture and forward the Query to GraphERRQL for LiveMode logging. The 'extensions' prop on GraphQL return statement captures and forwards the GraphQL response. Both cases utilize SSE connection setup by 'eventsHandler'
+app.use('/graphql', grapherrql(graphqlHTTP, schema));
+
+//Setup SSE (Server-Sent-Events) endpoint. GraphERRQL will setup conn to this upon initial render, connection will persist.
+app.get('/events', eventsHandler);
+//Referencing local files to serve to GraphERRQL. Deployment will see Host Apps referencing node-modules dynamically rather than looking for these files locally.
+const directoryPath = '../../build';
+app.use(express.static(directoryPath));
+//serve up grapherrql GUI & trigger SSE connection when opening browser to /grapherrql endpoint
+const defaultFilePath = '../../build/index.html';
+app.get('/grapherrql', serveGrapherrql(PORT, defaultFilePath));
+
+app.listen(PORT, () => console.log(`Server Running on ${PORT}`));
